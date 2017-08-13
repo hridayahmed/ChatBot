2826ebe7580be7d0749a8f63d0a93c783c270882
hridayahmed <hridayahmed22@gmail.com>
2017-08-13 10:15:01 +0600

    Simple Chatbot for AI project

---
+++ app/src/main/java/com/hridha/chatbot/list_item_message_recv.java
@@ -0,0 +1,13 @@
+package com.hridha.chatbot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class list_item_message_recv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item_message_recv);
+    }
+}
